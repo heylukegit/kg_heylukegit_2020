70c6512911662e05efe7e006bffac99f7a37524f
luke <mh3994@columbia.edu>
2020-03-14 17:53:50 -0400

    add some comments to main.py

--- main.py
+++ main.py
@@ -17,16 +17,18 @@ def isOneToOneMappingExists(s1, s2):
     :param s2: string
     :return: bool
     """
-
+    # init two sets
     set1 = set()
     set2 = set()
 
+    # store all the chars in a set
     for c in s1:
         set1.add(c)
 
     for c in s2:
         set2.add(c)
 
+    # use the length of set to check
     if len(set1) >= len(set2):
         return True
     else:
@@ -46,9 +48,11 @@ def test():
 if __name__ == "__main__":
     # test()
 
+    # read the strings from sys.argv
     s1 = sys.argv[1]
     s2 = sys.argv[2]
 
+    # call the function
     if isOneToOneMappingExists(s1, s2):
         print("true")
     else:
